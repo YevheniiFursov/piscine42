75fa60bf517868532a6408c2ec7f0a0d6935fa01
Yevhenii Fursov <yevheniifursov@MBP-Yevhenii.fritz.box>
2023-09-29 17:16:50 +0200

    test

---
+++ c00/ft_putchar.c
@@ -0,0 +1,16 @@
+include(stdio.h)
+int main (int ac, char **av)
+{
+    void ac;
+    void av;
+    write(1, "smert rusni"/n, 14);
+}
+
+int ft_strlen(char *str)
+{
+    int i;
+    i = 0;
+    if(str[i] != '\0')
+        i++;
+    return (i);
+}
